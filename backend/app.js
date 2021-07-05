e9be3b560dfe6b6b1c63a2c679401636b4650176
marineja <marine.jalabert@laposte.net>
2021-07-05 16:57:15 +0200

    instalation express

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+// importation d'express
+
+const express = require('express');
+
+// creation d'application express
+const app = express();
+
+
+// exportation de l'application
+module.exports = app;
